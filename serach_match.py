7cce85ffa1de966c9ab1e46078ae4f7de1b05759
unknown <taojiang0101@gmail.com>
2014-12-21 01:06:12 +0800

    rm 2013-BigData.txt

---
+++ serach_match.py
@@ -0,0 +1,24 @@
+# ===============================================================================
+#
+#       Filename: serach_match.py
+#
+#    Description: 
+#
+#        Version: 1.0
+#        Created: 2013/8/2 21:41:47
+#       Revision: none
+#       Compiler: gcc
+#
+#         Author: qingchen
+#      	   Email: taojiang0101@gmail.com
+#   Organization: 
+#
+# ===============================================================================
+
+#!/usr/bin/env python
+
+import urllib2
+content =  
+USRNAME = '772612340@qq.com'
+PASSWD = '78jtopq95'
+url = 'http:weibo.com.cn'
